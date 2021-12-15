2207f53f84d8a461973c14a5a12490e4fda85922
katada93 <tsokaev93@mail.ru>
2021-12-15 19:32:48 +0300

    delete import

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -1,5 +1,5 @@
 import { TagProps } from './Tag.props';
-import styles from './Tag.module.css';
+// import styles from './Tag.module.css';
 
 export const Tag = ({ children, ...props }: TagProps): JSX.Element => {
   return <div {...props}>{children}</div>;
